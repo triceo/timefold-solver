284afed3a231660b3bd999f8479b4f7301d9d6e0
Lukáš Petrovický <lukas@petrovicky.net>
2025-04-30 13:39:55 +0200

    chore: fix locale tests

--- core/src/test/java/ai/timefold/solver/core/api/solver/ProblemSizeStatisticsTest.java
+++ core/src/test/java/ai/timefold/solver/core/api/solver/ProblemSizeStatisticsTest.java
@@ -17,18 +17,16 @@ class ProblemSizeStatisticsTest {
                 Math.log10(scale));
     }
 
-    private static Locale defaultLocaleToRestore;
+    private static final Locale defaultLocaleToRestore = Locale.getDefault();
 
     @BeforeAll
-    public static void setLocale() {
-        defaultLocaleToRestore = Locale.getDefault();
+    public static synchronized void setLocale() {
         Locale.setDefault(Locale.US);
     }
 
     @AfterAll
-    public static void restoreLocale() {
+    public static synchronized void restoreLocale() {
         Locale.setDefault(defaultLocaleToRestore);
-        defaultLocaleToRestore = null;
     }
 
     @Test
@@ -40,6 +38,53 @@ class ProblemSizeStatisticsTest {
         statistics = getProblemSizeStatistics(250L);
         assertThat(statistics.approximateProblemScaleLogAsFixedPointLong())
                 .isEqualTo(MathUtils.getScaledApproximateLog(MathUtils.LOG_PRECISION, 10L, 250L));
+    }
+
+    @Test
+    void formatApproximateProblemScaleLocaled() {
+        var statistics = getProblemSizeStatistics(100L);
+        assertThat(statistics.approximateProblemScaleAsFormattedString(Locale.FRENCH))
+                .isEqualTo("100");
+
+        statistics = getProblemSizeStatistics(250L);
+        assertThat(statistics.approximateProblemScaleAsFormattedString(Locale.FRENCH))
+                .isEqualTo("250");
+
+        statistics = getProblemSizeStatistics(1_234_567L);
+        assertThat(statistics.approximateProblemScaleAsFormattedString(Locale.FRENCH))
+                .isEqualTo("1 234 567");
+
+        statistics = getProblemSizeStatistics(123_456_789L);
+        assertThat(statistics.approximateProblemScaleAsFormattedString(Locale.FRENCH))
+                .isEqualTo("123 456 789");
+
+        statistics = getProblemSizeStatistics(1_123_456_789L);
+        assertThat(statistics.approximateProblemScaleAsFormattedString(Locale.FRENCH))
+                .isEqualTo("1 123 456 789");
+
+        statistics = getProblemSizeStatistics(321_123_456_789L);
+        assertThat(statistics.approximateProblemScaleAsFormattedString(Locale.FRENCH))
+                .isEqualTo("3,211235 × 10^11");
+
+        // scale = -infinity
+        statistics = new ProblemSizeStatistics(0L, 0L, 0L, Double.NEGATIVE_INFINITY);
+        assertThat(statistics.approximateProblemScaleAsFormattedString(Locale.FRENCH))
+                .isEqualTo("0");
+
+        // scale = +infinity
+        statistics = new ProblemSizeStatistics(0L, 0L, 0L, Double.POSITIVE_INFINITY);
+        assertThat(statistics.approximateProblemScaleAsFormattedString(Locale.FRENCH))
+                .isEqualTo("0");
+
+        // scale = NaN
+        statistics = new ProblemSizeStatistics(0L, 0L, 0L, Double.NaN);
+        assertThat(statistics.approximateProblemScaleAsFormattedString(Locale.FRENCH))
+                .isEqualTo("0");
+
+        // scale = 0
+        statistics = new ProblemSizeStatistics(0L, 0L, 0L, 0);
+        assertThat(statistics.approximateProblemScaleAsFormattedString(Locale.FRENCH))
+                .isEqualTo("1");
     }
 
     @Test
@@ -88,4 +133,5 @@ class ProblemSizeStatisticsTest {
         assertThat(statistics.approximateProblemScaleAsFormattedString())
                 .isEqualTo("1");
     }
+
 }
